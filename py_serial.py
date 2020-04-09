5dc8e72d16d0ce16a2a981fbd2830b53bb14880f
Douglas Esteves <douglas.brsoftware@gmail.com>
2020-04-09 12:30:41 -0300

    Update py_serial.py

--- py_serial.py
+++ py_serial.py
@@ -4,13 +4,14 @@ from datetime import datetime
 import serial
 
 serial_ob = serial.Serial(port='COM5', baudrate=115200)
-filename = datetime.now().strftime("datalog_%Y%m%d_%H%M%S")
+serial_ob.write(b'AT+START\n\r')
+
+filename = datetime.now().strftime("datalog_%Y%m%d_%H%M%S.log")
 
 try:
     while True:
         serial_log = open(filename + '.csv','a')
         serial_log.write(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f,"))
-
                 +str(serial_ob.readline()))
         serial_log.close()
 except KeyboardInterrupt:
