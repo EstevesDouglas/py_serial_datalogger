d6c90792a65643ad051d26855b316f2c93b7ee64
Douglas Esteves <douglas.brsoftware@gmail.com>
2020-05-09 14:32:51 -0300

    Update py_serial.py

--- py_serial.py
+++ py_serial.py
@@ -3,20 +3,60 @@
 from datetime import datetime
 import serial
 import time
-serial_ob = serial.Serial(port='COM5', baudrate=115200)
+import keyboard
+
+#Mudar port de acordo com a porta serial do gateway
+COMport = 'COM3'
+COMspeed = 115200
 
 filename = datetime.now().strftime('datalog_%Y%m%d_%H%M%S')
+serial_log = open(filename + '.csv','a')
+
+#serial_ob = serial.Serial(port='COM5', baudrate=115200)
+serial_ob = serial.Serial(port=COMport,baudrate=COMspeed)
 
 time.sleep( 1 )
 serial_ob.write(b'AT+START\r')
-time.sleep( 1 )
+
+while True:  # making a loop      
+    try:  # used try so that if user pressed other than the given key error will not be shown
+
+        serial_log.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f,'))
+                +str(serial_ob.readline()))
+
+        if keyboard.is_pressed("q"):  # if key 'q' is pressed 
+            print('You Pressed A Key!')
+            print('Finalizando a coleta dos dados...')
+            serial_ob.write(b'AT+STOP\r')
+            serial_ob.close()
+            serial_log.write('### FIM DA COLETA ###')
+            serial_log.close()            
+            break  # finishing the loop
+    except:
+        print('Finalizando a coleta dos dados...')
+        break  # if user pressed a key other than the given key the loop will break
 
 try:
     while True:
-        serial_log = open(filename + '.csv','a')
-        serial_log.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f,'))
-
-                +str(serial_ob.readline()))
-        serial_log.close()
+        
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed("q"):  # if key 'q' is pressed 
+                print('Finalizando a coleta dos dados...')
+                serial_ob.write(b'AT+STOP\r')
+                serial_ob.close()
+                serial_log.write('### FIM DA COLETA ###')
+                serial_log.close()
+                break  # finishing the loop
+        except:
+            break  # if user pressed a key other than the given key the loop will break
+        
 except KeyboardInterrupt:
-    pass
+    print('Finalizando a coleta dos dados...')
+    serial_ob.write(b'AT+STOP\r')
+    serial_ob.close()
+    serial_log.write('### FIM DA COLETA ###')
+    serial_log.close()
+except:
+    print ("?")
+finally:
+    print ("Closing....")
